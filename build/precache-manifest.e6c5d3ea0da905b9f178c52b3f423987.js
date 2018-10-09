b80f35359c94f5616c247746871cc49e89ae85ba
Vu Nguyen <trachanh@gmail.com>
2018-10-09 16:21:03 +0300

    add build folder to git

---
+++ build/precache-manifest.e6c5d3ea0da905b9f178c52b3f423987.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f50e6810784b770d44a8",
+    "url": "/static/js/main.f50e6810.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "f50e6810784b770d44a8",
+    "url": "/static/css/main.ee67b536.chunk.css"
+  },
+  {
+    "revision": "6084edc0d2343f3d6b956d3665e23301",
+    "url": "/index.html"
+  }
+];
