efe1cd75384a3076c5999124dae912942ccde1b3
Vu Nguyen <trachanh@gmail.com>
2018-10-10 15:27:30 +0300

    not using express, let's see

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// var http = require('http');
+var http = require('http');
 
-// var server = http.createServer(function(request, response) {
+var server = http.createServer(function(request, response) {
 
-//     response.writeHead(200, {"Content-Type": "text/plain"});
-//     response.end("Hello World!");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello New World!");
 
-// });
-app.use(express.static('client/build'))
-app.get('*', (req, res) => {
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Hello World!");
-    // res.sendFile('client/build/index.html');
-})
-app.listen(port);
+});
+// app.use(express.static('client/build'))
+// app.get('*', (req, res) => {
+//     res.writeHead(200, {"Content-Type": "text/plain"});
+//     res.end("Hello World!");
+//     // res.sendFile('client/build/index.html');
+// })
+// app.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
