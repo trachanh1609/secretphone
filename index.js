765ea772c74172b9e94dafa5f94f4b1a9ba7d079
Vu Nguyen <trachanh@gmail.com>
2018-10-10 15:42:36 +0300

    use var instead of const

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
 
-var http = require('http');
+// var http = require('http');
 
-var server = http.createServer(function(request, response) {
+// var server = http.createServer(function(request, response) {
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello New World!");
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.end("Hello New World!");
 
-});
+// });
 
-server.listen(port);
+// server.listen(port);
 
-// app.use(express.static('client/build'))
-// app.get('*', (req, res) => {
-//     res.writeHead(200, {"Content-Type": "text/plain"});
-//     res.end("Hello World!");
-//     // res.sendFile('client/build/index.html');
-// })
-// app.listen(port);
+app.use(express.static('client/build'))
+app.get('*', (req, res) => {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.end("Hello Another World!");
+    // res.sendFile('client/build/index.html');
+})
+app.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
